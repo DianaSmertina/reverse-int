0f697092e83c99a82fe0cff581931bc3d4d13f9f
DianaSmertina <am.dianasmertina@gmail.com>
2022-08-11 01:03:08 +0400

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    const arr = String(n).split('');
+    const arrReverse = arr.reverse();
+    while (arrReverse[0] === 0) {
+        arrReverse.shift();
+    }
+    return Number(arrReverse.join(''));
 }
